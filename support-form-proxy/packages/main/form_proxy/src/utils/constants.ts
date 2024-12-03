cb5709be02f725d11208ed34a422540e7e996727
SharbelOkzan <sharbelpib@gmail.com>
2024-12-03 13:33:43 +0100

    added new field IDs

--- support-form-proxy/packages/main/form_proxy/src/utils/constants.ts
+++ support-form-proxy/packages/main/form_proxy/src/utils/constants.ts
@@ -2,12 +2,38 @@ export const baseURL = "https://support.wecourts.com/hc/en-us/requests/new";
 export const formId = "22267967949597";
 
 export const fieldIds = {
-    reasonForContact: "22268061904029",  // Reason for contacting
-    fullName: "22354923742493",         // Full name
-    dateOfBirth: "22354888248733",      // Date of birth
-    nationality: "22354979580445",      // Nationality
-    gender: "22355080909597",           // Gender
-    city: "22355001638557",             // City
-    profileLink: "22355158811293",      // Profile link
-    phoneNumber: "22429069854621",      // Phone number
+
+    "address": "22488446465437",  //Drop-down     
+    "assignee": "22230557536285",  //Drop-down     
+    "checkbox-for-reporting-issue-about-another-player": "23355254801693",  //Checkbox     
+    "club-name": "23444496991261",  //Text     
+    "customer-email": "23329972513821",  //Regex     
+    "customer-name": "23329977221917",  //Text     
+    "customer-s-correct-name": "22290619926685",  //Text     
+    "date-of-birth": "22354888248733",  //Date     
+    "description": "22230573217437",  //Multi-line     
+    "full-name": "22354923742493",  //Text     
+    "gender": "22355080909597",  //Drop-down     
+    "group": "22230557536157",  //Drop-down     
+    "issue-type": "22268061904029",  //Drop-down     
+    "nationality": "22431803712413",  //Drop-down     
+    "player-name": "22291179605789",  //Text     
+    "priority": "22230557535517",  //Drop-down     
+    "reported-player-s-wppr-link": "23355294331805",  //Regex     
+    "subject": "22230573217309",  //Text     
+    "ticket-status": "22230557534237",  //Drop-down     
+    "topic": "22230619568669",  //Drop-down     
+    "tournament-date": "23444458781213",  //Date     
+    "tournament-name": "23444443586845",  //Text     
+    "type": "22230573222045",  //Drop-down     
+    "whats-app-number": "22429069854621",  //Regex     
+    "wppr-id": "22268471295133",  //Regex     
+    "wppr-link": "22355158811293",  //Regex     
+    "wppr-link-already-claimed": "22486940093597",  //Regex     
+    "wppr-link-dupe-irrelevant-links": "22467689064605",  //Multi-line     
+    "wppr-link-dupe-primary": "22467567956253",  //Regex     
+    "wppr-rating-feedback": "22468881695773",  //Checkbox     
+    "wrong-match-data": "22467817435037",  //Checkbox     
+    "wrong-player-name-s": "22468044764317",  //Checkbox     
+    "your-correct-score": "22467904516125",  //Numeric     
 };
