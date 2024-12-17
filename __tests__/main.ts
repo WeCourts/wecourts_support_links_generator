28a1cc4516a380c3b64f0d6881ee8463840e8219
SharbelOkzan <sharbelpib@gmail.com>
2024-12-17 11:39:53 +0100

    ready with tests
    (squashed commits)

--- __tests__/main.ts
+++ __tests__/main.ts
@@ -1,19 +1,68 @@
-// import { main } from '../support-form-proxy/packages/main/form_proxy/src/main';
+import { generateLink, main } from '../support-form-proxy/packages/main/form_proxy/src/main';
 
-// describe('main function', () => {
+describe('generateLink', () => {
+    it('should generate a link for the "technical-issue" form', () => {
+        const args = {
+            "form-name": "technical-issue",
+            "wppr-id": "12345",
+            "whatsapp-number": "9876543210",
+            "platform": "ios",
+            "version": "1.0.0"
+        };
+        const expectedLink = "https://support.wecourts.com/hc/en-us/requests/new?ticket_form_id=23468162626461&tf_22355158811293=https%3A%2F%2Fwecourts.com%2Fwppr%2F12345&tf_22429069854621=9876543210&tf_23468234820637=ios&tf_23468244354845=1.0.0";
+        expect(generateLink(args)).toBe(expectedLink);
+    });
 
-//     test('should return link for wrong_player_name issue type', () => {
-//         const args = {
-//             issueType: 'wrong_player_name',
-//             profileLink: 'profileLinkValue',
-//             fullName: 'fullNameValue',
-//         };
-//         const result = main(args);
-//         expect(result['body']).toContain('https://mysite.zendesk.com/hc/en-us/requests/new');
-//         expect(result['body']).toContain('tf_22355158811293=profileLinkValue');
-//         expect(result['body']).toContain('tf_22354923742493=fullNameValue');
-//     });
-// });
+    it('should generate a link for the "someone-claimed-my-account" form', () => {
+        const args = {
+            "form-name": "someone-claimed-my-account",
+            "whatsapp-number": "9876543210",
+            "wpprIdOfConflictedAccount": "54321"
+        };
+        const expectedLink = "https://support.wecourts.com/hc/en-us/requests/new?ticket_form_id=23374031596189&tf_22429069854621=9876543210&tf_22486940093597=54321";
+        expect(generateLink(args)).toBe(expectedLink);
+    });
+
+    it('should generate a link for the "update-profile-information" form', () => {
+        const args = {
+            "form-name": "update-profile-information",
+            "wppr-id": "12345",
+            "whatsapp-number": "9876543210"
+        };
+        const expectedLink = "https://support.wecourts.com/hc/en-us/requests/new?ticket_form_id=23374053996061&tf_22355158811293=https%3A%2F%2Fwecourts.com%2Fwppr%2F12345&tf_22429069854621=9876543210";
+        expect(generateLink(args)).toBe(expectedLink);
+    });
+
+    it('should return the base URL if the form name is "something-else"', () => {
+        const args = {
+            "form-name": "something-else"
+        };
+        const expectedLink = "https://support.wecourts.com/hc/en-us/requests/new";
+        expect(generateLink(args)).toBe(expectedLink);
+    });
+
+    it('should return the base URL if the form name does not exist', () => {
+        const args = {
+            "form-name": "non-existent-form"
+        };
+        const expectedLink = "https://support.wecourts.com/hc/en-us/requests/new";
+        expect(generateLink(args)).toBe(expectedLink);
+    });
+});
+
+describe ('main function', () => {
+    it('should return link for technical issue', () => {
+        const args = {
+            'form-name': 'technical-issue',
+            'whatsapp-number': '9876543210',
+            'wppr-id': '12345',
+        };
+        const expectedLink = 'https://support.wecourts.com/hc/en-us/requests/new?ticket_form_id=23468162626461&tf_22355158811293=https%3A%2F%2Fwecourts.com%2Fwppr%2F12345&tf_22429069854621=9876543210';
+        const expectedResponse = { "body": expectedLink };
+        expect(main(args)).toEqual(expectedResponse);
+     
+    });
+});
 
 
 // describe('fetching the serverless function remotely', () => {
--- support-form-proxy/packages/main/form_proxy/src/main.ts
+++ support-form-proxy/packages/main/form_proxy/src/main.ts
@@ -1,31 +1,123 @@
+const baseUrl = "https://support.wecourts.com/hc/en-us/requests/new";
+
 export function main(args: {}): {} {
 
     console.log(args);
 
-    let formId;
+    let result = generateLink(args);
     
-    
-    if (args["form-name"] === "something-else") {
-        return { body: baseUrl };
-    }
-    formId = formIds[args["form-name"]] ?? formIds["something-else"];
-    let url = `${baseUrl}?ticket_form_id=${formId}`;
-    return { body: url };
+    return { body: result };
 
 }
 
-const baseUrl = "https://support.wecourts.com/hc/en-us/requests/new";
- const formIds = {
-    "community-reporting-channel": "23374000465821", // not used
-    "duplicate-profiles-in-wppr": "23374026229533",
-    "tournament-results-not-on-wecourts-yet": "23444411211165", // not used
-    "wrong-match-data": "23374037391133",
-    "report-rating-issue": "23374008389533",
-    "update-profile-information": "23374053996061",
-    "someone-claimed-my-account": "23374031596189",
-    "become-wppr-partner": "23374076480157", // not used
-    "add-tournament-to-wecourts": "23374072567581", // not used
-    "submit-tournament-results": "23374073574685", // not used
-    "technical-issue": "23468162626461",
-    "something-else": "23374085333277",
+
+function generateSupportTicketLink(baseURL: string, formId: string, fields: Record<string, string>): string {
+    const params = new URLSearchParams({
+        "ticket_form_id": formId,
+        ...Object.fromEntries(
+            Object.entries(fields).map(([key, value]) => [`tf_${key}`, value])
+        ),
+    });
+
+    return `${baseURL}?${params.toString()}`;
+}
+
+class Form {
+    id : string;
+    serializedName : string;
+    fields : FormField[];
+    
+    constructor(id: string, serializedName : string, fields: FormField[]) {
+        this.id = id;
+        this.serializedName = serializedName;
+        this.fields = fields;
+    }
+    
+    extractValues(args : {}): Record<string, string> {
+        let result = {};
+        for (let field of this.fields) {
+            if (field.extractValue(args) !== undefined)
+            {
+                result[field.id] = field.extractValue(args);
+            }
+        }
+        return result;
+    }
+}
+class FormField {
+ id : string;
+ zendeskName : string;
+ protected mobileClientName : string;
+
+ constructor(id: string, mobileClientName: string, zendeskName: string) {
+        this.id = id;
+        this.mobileClientName = mobileClientName;
+        this.zendeskName = zendeskName;
+    }
+
+    extractValue(args : {}): string {
+        return args[this.mobileClientName];
+    }
+}
+
+ class DropDownFormField extends FormField {
+ options : Record<string, string>;
+
+ constructor (id: string, mobileClientName: string, zendeskName: string, options: Record<string, string>) {
+        super(id, mobileClientName, zendeskName);
+        this.options = options;
+    }
+
+    extractValue(args : {}): string {
+        let value = args[this.mobileClientName];
+        return this.options[value];
+    }
+}
+
+class WpprLinkFormField extends FormField {
+    constructor() {
+        super("22355158811293", "wppr-id" ,"wppr-link");}
+    
+    extractValue(args : {}): string {
+        let value = args[this.mobileClientName];
+        return "https://wecourts.com/wppr/" + value;
+    }
+}
+
+const platformField = new DropDownFormField("23468234820637", "platform", "platform", {
+    "ios": "ios",
+    "android": "android",
+    "web": "web",
+}); 
+
+const wpprLinkField = new WpprLinkFormField();
+
+const whatsAppNumberField = new FormField("22429069854621", "whatsapp-number", "whats-app-number");
+
+const appVersionField = new FormField("23468244354845", "version", "app-version");
+const wpprLinkAlreadyClaimed = new FormField("22486940093597", "wpprIdOfConflictedAccount", "wppr-link-already-claimed");
+
+const updateProfileInformationForm = new Form("23374053996061", "update-profile-information", [wpprLinkField, whatsAppNumberField,]);
+const someoneClaimedMyAccountForm = new Form("23374031596189", "someone-claimed-my-account", [whatsAppNumberField, wpprLinkAlreadyClaimed,]);
+const technicalIssueForm = new Form("23468162626461", "technical-issue" ,[wpprLinkField, whatsAppNumberField, platformField, appVersionField,]);
+
+const forms = [technicalIssueForm, someoneClaimedMyAccountForm, updateProfileInformationForm,];
+
+const formMap = Object.fromEntries(forms.map(form => [form.serializedName, form]));
+
+export function generateLink(args: {}): string {
+    
+    let result = baseUrl;
+    
+    let isSomethingElse = args["form-name"] === "something-else";
+    let exist = formMap[args["form-name"]];
+    
+    if (exist && !isSomethingElse) {
+
+        let form = exist;
+        let fields = form.extractValues(args);
+        result = generateSupportTicketLink(baseUrl, form.id, fields);
+    }
+
+    return result ;
 }
